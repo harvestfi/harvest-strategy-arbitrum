5c816f69a270690a953bcc6fa1eebb4939f8d04d
CryptJS13 <jasper@slimmens.nl>
2023-03-11 15:48:18 +0100

    Add poison strategies

---
+++ test/poison/pslvr.js
@@ -0,0 +1,185 @@
+// Utilities
+const Utils = require("../utilities/Utils.js");
+const { impersonates, setupCoreProtocol, depositVault } = require("../utilities/hh-utils.js");
+
+const addresses = require("../test-config.js");
+const { send } = require("@openzeppelin/test-helpers");
+const BigNumber = require("bignumber.js");
+const IERC20 = artifacts.require("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20");
+
+const HodlStrategy = artifacts.require("PoisonStrategyiPoisonMainnet_iPoison");
+const Strategy = artifacts.require("PoisonStrategyHodlMainnet_pSLVR");
+
+const D18 = new BigNumber(Math.pow(10, 18));
+
+//This test was developed at blockNumber 68798200
+
+// Vanilla Mocha test. Increased compatibility with tools that integrate Mocha.
+describe("Mainnet Jarvis pSLVR HODL in iPoison", function() {
+  let accounts;
+
+  // external contracts
+  let underlying;
+
+  // external setup
+  let underlyingWhale = "0x99655CA16C742b46A4a05AFAf0f7798C336Fd279";
+  let hodlUnderlying = "0xDA016d31f2B52C73D7c1956E955ae8A507b305bB";
+
+  // parties in the protocol
+  let governance;
+  let farmer1;
+
+  // numbers used in tests
+  let farmerBalance;
+
+  // Core protocol contracts
+  let controller;
+  let vault;
+  let strategy;
+
+  async function setupExternalContracts() {
+    underlying = await IERC20.at("0x867B1Cd06039Eb70385788a048B57F6d4fDC5Dbb");
+    console.log("Fetching Underlying at: ", underlying.address);
+  }
+
+  async function setupBalance(){
+    let etherGiver = accounts[9];
+    // Give whale some ether to make sure the following actions are good
+    await send.ether(etherGiver, underlyingWhale, "1" + "000000000000000000");
+
+    farmerBalance = await underlying.balanceOf(underlyingWhale);
+    await underlying.transfer(farmer1, farmerBalance, { from: underlyingWhale });
+  }
+
+  before(async function() {
+    governance = addresses.Governance;
+    accounts = await web3.eth.getAccounts();
+
+    farmer1 = accounts[1];
+
+    // impersonate accounts
+    await impersonates([governance, underlyingWhale]);
+    let etherGiver = accounts[9];
+    await send.ether(etherGiver, governance, "100" + "000000000000000000");
+
+    await setupExternalContracts();
+    [controller, hodlVault, hodlStrategy] = await setupCoreProtocol({
+      "existingVaultAddress": null,
+      "strategyArtifact": HodlStrategy,
+      "strategyArtifactIsUpgradable": true,
+      "underlying": await IERC20.at(hodlUnderlying),
+      "governance": governance,
+    });
+    [controller, vault, strategy, potPool] = await setupCoreProtocol({
+      "existingVaultAddress": null,
+      "strategyArtifact": Strategy,
+      "strategyArtifactIsUpgradable": true,
+      "underlying": underlying,
+      "governance": governance,
+      "rewardPool" : true,
+      "rewardPoolConfig": {
+        type: 'PotPool',
+        rewardTokens: [
+          hodlVault.address // fiPoison
+        ]
+      },
+    });
+
+    await strategy.setHodlVault(hodlVault.address, {from: governance});
+    await strategy.setPotPool(potPool.address, {from: governance});
+    await potPool.setRewardDistribution([strategy.address], true, {from: governance});
+    await controller.addToWhitelist(strategy.address, {from: governance});
+
+    // whale send underlying to farmers
+    await setupBalance();
+  });
+
+  describe("Happy path", function() {
+    it("Farmer should earn money", async function() {
+      let farmerOldBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      let farmerOldHodlBalance = new BigNumber(await hodlVault.balanceOf(farmer1));
+      await depositVault(farmer1, underlying, vault, farmerBalance);
+      let fTokenBalance = new BigNumber(await vault.balanceOf(farmer1));
+
+      let erc20Vault = await IERC20.at(vault.address);
+      await erc20Vault.approve(potPool.address, fTokenBalance, {from: farmer1});
+      await potPool.stake(fTokenBalance, {from: farmer1});
+
+      // Using half days is to simulate how we doHardwork in the real world
+      let hours = 10;
+      let blocksPerHour = 3600;
+      let oldSharePrice;
+      let newSharePrice;
+      let oldHodlSharePrice;
+      let newHodlSharePrice;
+      let oldPotPoolBalance;
+      let newPotPoolBalance;
+      let hodlPrice;
+      let lpPrice;
+      let oldValue;
+      let newValue;
+      for (let i = 0; i < hours; i++) {
+        console.log("loop ", i);
+
+        oldSharePrice = new BigNumber(await vault.getPricePerFullShare());
+        oldHodlSharePrice = new BigNumber(await hodlVault.getPricePerFullShare());
+        oldPotPoolBalance = new BigNumber(await hodlVault.balanceOf(potPool.address));
+        await controller.doHardWork(vault.address, {from: governance});
+        await controller.doHardWork(hodlVault.address, {from: governance});
+        newSharePrice = new BigNumber(await vault.getPricePerFullShare());
+        newHodlSharePrice = new BigNumber(await hodlVault.getPricePerFullShare());
+        newPotPoolBalance = new BigNumber(await hodlVault.balanceOf(potPool.address));
+
+        hodlPrice = new BigNumber(0.556480).times(D18);
+        underlyingPrice = new BigNumber(1858.14).times(D18);
+        console.log("Hodl price:", hodlPrice.toFixed()/D18.toFixed());
+        console.log("Underlying price:", underlyingPrice.toFixed()/D18.toFixed());
+
+        oldValue = (fTokenBalance.times(oldSharePrice).times(underlyingPrice)).div(1e36).plus((oldPotPoolBalance.times(oldHodlSharePrice).times(hodlPrice)).div(1e36));
+        newValue = (fTokenBalance.times(newSharePrice).times(underlyingPrice)).div(1e36).plus((newPotPoolBalance.times(newHodlSharePrice).times(hodlPrice)).div(1e36));
+
+        console.log("old value: ", oldValue.toFixed()/D18.toFixed());
+        console.log("new value: ", newValue.toFixed()/D18.toFixed());
+        console.log("growth: ", newValue.toFixed() / oldValue.toFixed());
+
+        console.log("Hodl token in potpool: ", newPotPoolBalance.toFixed());
+
+        apr = (newValue.toFixed()/oldValue.toFixed()-1)*(24/(blocksPerHour/300))*365;
+        apy = ((newValue.toFixed()/oldValue.toFixed()-1)*(24/(blocksPerHour/300))+1)**365;
+
+        console.log("instant APR:", apr*100, "%");
+        console.log("instant APY:", (apy-1)*100, "%");
+
+        await Utils.advanceNBlock(blocksPerHour);
+      }
+      // withdrawAll to make sure no doHardwork is called when we do withdraw later.
+      await vault.withdrawAll({ from: governance });
+
+      // wait until all reward can be claimed by the farmer
+      await Utils.waitTime(86400 * 30 * 1000);
+      console.log("vaultBalance: ", fTokenBalance.toFixed());
+      await potPool.exit({from: farmer1});
+      await vault.withdraw(fTokenBalance.toFixed(), { from: farmer1 });
+      let farmerNewBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      let farmerNewHodlBalance = new BigNumber(await hodlVault.balanceOf(farmer1));
+      Utils.assertBNGte(farmerNewBalance, farmerOldBalance);
+      Utils.assertBNGt(farmerNewHodlBalance, farmerOldHodlBalance);
+
+      oldValue = (fTokenBalance.times(1e18).times(underlyingPrice)).div(1e36);
+      newValue = (fTokenBalance.times(newSharePrice).times(underlyingPrice)).div(1e36).plus((farmerNewHodlBalance.times(newHodlSharePrice).times(hodlPrice)).div(1e36));
+
+      apr = (newValue.toFixed()/oldValue.toFixed()-1)*(24/(blocksPerHour*hours/300))*365;
+      apy = ((newValue.toFixed()/oldValue.toFixed()-1)*(24/(blocksPerHour*hours/300))+1)**365;
+
+      console.log("Overall APR:", apr*100, "%");
+      console.log("Overall APY:", (apy-1)*100, "%");
+
+      console.log("potpool totalShare: ", (new BigNumber(await potPool.totalSupply())).toFixed());
+      console.log("Hodl token in potpool: ", (new BigNumber(await hodlVault.balanceOf(potPool.address))).toFixed() );
+      console.log("Farmer got hodl token from potpool: ", farmerNewHodlBalance.toFixed());
+      console.log("earned!");
+
+      await strategy.withdrawAllToVault({ from: governance }); // making sure can withdraw all for a next switch
+    });
+  });
+});
