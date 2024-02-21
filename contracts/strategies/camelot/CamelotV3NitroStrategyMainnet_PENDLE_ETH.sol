8e51b282fb2cc16d101325237bec111e25f3eb1a
CryptJS13 <cryptjs13@protonmail.com>
2024-02-21 16:07:18 +0100

    Add camelotV3 pools

---
+++ contracts/strategies/camelot/CamelotV3NitroStrategyMainnet_PENDLE_ETH.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.6.12;
+
+import "./CamelotV3NitroStrategy.sol";
+
+contract CamelotV3NitroStrategyMainnet_PENDLE_ETH is CamelotV3NitroStrategy {
+
+  constructor() public {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0x05F3ABD2B008e8b36a410F3C42E703cB776CeCD0);
+    address grail = address(0x3d9907F9a368ad0a51Be60f7Da3b97cf940982D8);
+    address arb = address(0x912CE59144191C1204E64559FE8253a0e49E6548);
+    address nftPool = address(0x2d1334195Ce78a4dCFe28005f8275b9703129E75);
+    address nitroPool = address(0x32535535d4C5442ddFD9B7d14CEf458F4B94A4ec);
+    CamelotV3NitroStrategy.initializeBaseStrategy(
+      _storage,
+      underlying,
+      _vault,
+      grail,
+      nftPool,
+      nitroPool,
+      address(0xFA10759780304c2B8d34B051C039899dFBbcad7f), //xGrail vault
+      address(0), //PotPool
+      address(0x1F1Ca4e8236CD13032653391dB7e9544a6ad123E) //UniProxy
+    );
+    rewardTokens = [grail, arb];
+  }
+}
