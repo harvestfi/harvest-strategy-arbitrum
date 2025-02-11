97269f4d106a01a752665cbbf983aa283b904e9a
CryptJS13 <cryptjs13@protonmail.com>
2025-02-11 13:47:43 +0100

    Merged solc0.8 upgrade

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       forking: {
         url: `https://arb-mainnet.g.alchemy.com/v2/${secret.alchemyKey}`,
-        blockNumber: 286693815, // <-- edit here
+        blockNumber: 300500460, // <-- edit here
       },
     },
     mainnet: {
