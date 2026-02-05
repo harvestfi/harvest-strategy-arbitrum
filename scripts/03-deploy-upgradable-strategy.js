1e453cfb6a38894fbb2784968b8b893695749540
CryptJS13 <cryptjs13@protonmail.com>
2026-02-05 08:35:35 +0100

    Update scripts

--- scripts/03-deploy-upgradable-strategy.js
+++ scripts/03-deploy-upgradable-strategy.js
@@ -14,11 +14,13 @@ async function main() {
   const impl = await type2Transaction(StrategyImpl.new);
 
   console.log("Implementation deployed at:", impl.creates);
+  await new Promise(r => setTimeout(r, 2000))
 
   const StrategyProxy = artifacts.require('StrategyProxy');
   const proxy = await type2Transaction(StrategyProxy.new, impl.creates);
 
   console.log("Proxy deployed at:", proxy.creates);
+  await new Promise(r => setTimeout(r, 2000))
 
   const strategy = await StrategyImpl.at(proxy.creates);
   await type2Transaction(strategy.initializeStrategy, addresses.Storage, vaultAddr);
@@ -30,6 +32,11 @@ async function main() {
   "newStrategy": "${proxy.creates}"
 }`
   )
+  try {
+    await hre.run("verify:verify", {address: impl.creates}); 
+  } catch (e) {
+    console.log("Verification failed:", e);
+  }
 }
 
 main()
